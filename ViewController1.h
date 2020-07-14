66d3794a3ba2c2f32d6ac29f2d4e178886091ed9
GanggangGao <struggle3g@126.com>
2020-07-14 20:44:12 +0800

    3gtest1

---
+++ ViewController1.h
@@ -0,0 +1,17 @@
+//
+//  ViewController1.h
+//  3gComponentDemo
+//
+//  Created by zachary spark on 2020/7/14.
+//  Copyright © 2020 Struggle3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController1 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ViewController1.m
@@ -0,0 +1,31 @@
+//
+//  ViewController1.m
+//  3gComponentDemo
+//
+//  Created by zachary spark on 2020/7/14.
+//  Copyright © 2020 Struggle3g. All rights reserved.
+//
+
+#import "ViewController1.h"
+@interface ViewController1 ()
+
+@end
+
+@implementation ViewController1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
